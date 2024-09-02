a24e33542ae7807aabcd62ca1752d6aaca805f8f
Mithesh Shankar <131614258+mithesh06@users.noreply.github.com>
2024-09-02 11:05:01 +0530

    Update uninstall_ator.sh

--- ator/uninstall_ator.sh
+++ ator/uninstall_ator.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 sudo apt-get -y remove anon 
+sudo systemctl unmask anon.service
+sudo systemctl disable anon.service
+sudo systemctl stop anon.service
+sudo apt-get -y purge anon
+systemctl daemon-reload
